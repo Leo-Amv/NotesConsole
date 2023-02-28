c635fc48897ae84f788f3c5c3ed7614421730407
Leo_Amv <leonidamvrosov@yandex.ru>
2023-03-01 02:39:09 +0500

    funcs updated

--- funcs.py
+++ funcs.py
@@ -18,3 +18,60 @@ def create():
                       datetime.now().minute, datetime.now().second))
     create_note(title, body, date, time)
     print('Note created!')
+
+
+def update_success():
+    print('Enter changes:')
+    create()
+    print('Note updated!')
+
+
+def update_by_id():
+    try:
+        find_id = int(input('Enter id: '))
+        key = delete_note_by_id(find_id)
+        if key == 1:
+            update_success()
+    except ValueError:
+        print('Incorrect id, try again!')
+        update_by_id()
+
+
+def delete_by_id():
+    try:
+        find_id = int(input('Enter id: '))
+        key = delete_note_by_id(find_id)
+        if key == 1:
+            print('Note deleted!')
+    except ValueError:
+        print('Incorrect id, try again!')
+        delete_by_id()
+
+
+def find_by_id():
+    try:
+        find_id = int(input('Enter id: '))
+        flag = find_note_by_id(find_id)
+        if flag == 1:
+            print('--DONE--')
+    except ValueError:
+        print('Incorrect id, try again!')
+        find_by_id()
+
+
+def list_notes():
+    print('Show all notes: \n')
+    list_all_notes()
+
+
+def date_note():
+    print('Enter date to find note: ')
+    try:
+        day = int(input('Day: '))
+        month = int(input('Month: '))
+        year = int(input('Day: '))
+        print('Founded notes: ')
+        find_notes_by_date(day, month, year)
+    except ValueError:
+        print('Incorrect date, try again!')
+        date_note()
