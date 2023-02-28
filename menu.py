f1e108ff7233249aa79090eadc62aebeed588899
Leo_Amv <leonidamvrosov@yandex.ru>
2023-03-01 02:47:53 +0500

    menu updated

--- menu.py
+++ menu.py
@@ -4,22 +4,24 @@ from funcs import *
 def start():
     print('\t---This is note app!---')
     cmd_list()
-    command = input('Enter command:')
-    match str.upper(command):
-        case 'ADD':
-            create()
-        case 'UPDATE':
-            print('foo')
-        case 'LIST':
-            print('foo')
-        case 'DELETE':
-            print('foo')
-        case 'FIND':
-            print('foo')
-        case 'HELP':
-            cmd_list()
-        case 'EXIT':
-            exit()
-        case _:
-            print('Command not found! Try again!')
-            start()
+    flag = True
+    while (flag):
+        command = input('Enter command:')
+        match str.upper(command):
+            case 'ADD':
+                create()
+            case 'UPDATE':
+                update_by_id()
+            case 'LIST':
+                list_notes()
+            case 'DELETE':
+                delete_by_id()
+            case 'FIND':
+                find_by_id()
+            case 'HELP':
+                cmd_list()
+            case 'EXIT':
+                flag = False
+                exit()
+            case _:
+                print('Command not found! Try again!')
