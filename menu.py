527c3d326cec9dbb328434da52581d60b71ec6e7
Leo_Amv <leonidamvrosov@yandex.ru>
2023-03-01 03:04:30 +0500

    menu case find updated

--- menu.py
+++ menu.py
@@ -17,7 +17,18 @@ def start():
             case 'DELETE':
                 delete_by_id()
             case 'FIND':
-                find_by_id()
+                choise = 0
+                while (choise < 1 or choise > 3):
+                    choise = int(input(
+                        'Enter number to choose:\n"1"\t to find by id\n"2"\t to find by keyword\n"3"\t to find by date'))
+                    if choise == 1:
+                        find_by_id()
+                    elif choise == 2:
+                        find_by_keyword()
+                    elif choise == 3:
+                        find_by_date()
+                    else:
+                        print('Incorrect data, try again!')
             case 'HELP':
                 cmd_list()
             case 'EXIT':
